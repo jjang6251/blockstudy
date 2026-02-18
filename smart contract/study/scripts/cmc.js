335943de10306b9d63cb7fcffd606344531e3377
JjangSW <jjang6251@gmail.com>
2026-02-18 17:14:26 +0900

    컨트랙트 주소 변경

--- smart contract/study/scripts/cmc.js
+++ smart contract/study/scripts/cmc.js
@@ -1,46 +1,56 @@
 const axios = require('axios');
 const dotenv = require('dotenv');
 dotenv.config();
-const{ ethers } = require('hardhat');
+const { ethers } = require('ethers');
+const hre = require("hardhat");
 
 let response = null;
 new Promise(async (resolve, reject) => {
     try {
-        response = await axios.get('//코인마켓 api', {
+        response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC', {
             headers: {
                 'X-CMC_PRO_API_KEY': process.env.CMC_API_KEY,
             },
         });
-    } catch(ex) {
+    } catch (ex) {
         response = null;
 
         console.log(ex);
         reject(ex);
     }
-    if(response) {
+    if (response) {
         const json = response.data;
-        console.log(json);
 
         const bitcoin = json.data.BTC.find(coin => coin.name === "Bitcoin");
+        console.log(bitcoin);
 
         if (bitcoin && bitcoin.quote && bitcoin.quote.USD) {
             console.log("Bitcoin USD data: ");
-            console.log(JSON.stringify(bitcoin.quote.usd, null, 2));
+            console.log(JSON.stringify(bitcoin.quote.USD, null, 2));
             const realPrice = bitcoin.quote.USD.price;
             console.log("real price: ", realPrice);
 
-            const provider = new ethers.JsonRpcApiProvider(process.env.SEPOLIA_RPC_URL);
+            const provider = new ethers.JsonRpcProvider(process.env.SEPOLIA_RPC_URL);
             const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-            const artifact = await hre.artifact.readArtifact("FastcampusOracle");
+            const artifact = await hre.artifacts.readArtifact("FastcampusOracle");
             const abi = artifact.abi;
             const bytecode = artifact.bytecode;
-            const oracle = new ethers.Contract("0x~~", abi, wallet);
+            const oracle = new ethers.Contract("0x59098E4cF0B240752f80ad76bf92f0341ED801cE", abi, wallet);
             const ethersPrice = ethers.parseEther(realPrice.toString());
+            const owner = await oracle.owner();
+            const signerAddress = await wallet.getAddress();
+
+            console.log("컨트랙트 Owner:", owner);
+            console.log("현재 실행 지갑:", signerAddress);
+
+            if (owner.toLowerCase() !== signerAddress.toLowerCase()) {
+                console.error("에러: 현재 지갑은 권한이 없습니다!");
+            }
             const tx = await oracle.setPrice(ethersPrice);
             const reciept = await tx.wait();
-            console.log(receipt);
+            console.log(reciept);
             const price = await oracle.getPrice();
-            console.log("price:", price);
+            console.log("oracle get price:", price);
             resolve(bitcoin.quote.USD);
         } else {
             console.log("Bitcoin data not found in response");
