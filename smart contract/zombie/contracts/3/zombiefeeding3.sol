771f3ba14b86a242d7260b3171e64694540b3a68
JjangSW <jjang6251@gmail.com>
2026-02-27 18:37:56 +0900

    prepare lesson3

---
+++ smart contract/zombie/contracts/3/zombiefeeding3.sol
@@ -0,0 +1,75 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "./zombiefactory3.sol";
+
+interface KittyInterface {
+    function getKitty(
+        uint256 _id
+    )
+        external
+        view
+        returns (
+            bool isGestating,
+            bool isReady,
+            uint256 cooldownIndex,
+            uint256 nextActionAt,
+            uint256 siringWithId,
+            uint256 birthTime,
+            uint256 matronId,
+            uint256 sireId,
+            uint256 generation,
+            uint256 genes
+        );
+}
+
+contract ZombieFeeding3 is ZombieFactory3 {
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    function feedAndMultiply(
+        uint _zombieID,
+        uint _targetDna,
+        string memory _species
+    ) public {
+        require(zombieToOwner[_zombieID] == msg.sender);
+        Zombie storage myZombie = zombies[_zombieID];
+
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+        if (
+            keccak256(abi.encodePacked(_species)) ==
+            keccak256(abi.encodePacked("kitty"))
+        ) {
+            newDna = newDna - (newDna % 100) + 99;
+        }
+        _createZombie("NoName", newDna);
+    }
+
+    /**
+        function multipleReturns() internal returns(uint a, uint b, uint c) {
+            return(1,2,3);
+        }
+        function processMultipleReturns() external {
+            uint a;
+            uint b;
+            uint c;
+            // 다음과 같이 다수 값을 할당한다:
+            (a, b, c) = multipleReturns();
+        }
+
+        // 혹은 단 하나의 값에만 관심이 있을 경우: 
+        function getLastReturnValue() external {
+            uint c;
+            // 다른 필드는 빈칸으로 놓기만 하면 된다: 
+            (,,c) = multipleReturns();
+        }
+     */
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (, , , , , , , , , kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna, "kitty");
+    }
+}
