4d161ef760542e7086100ed364799d06f9fbb376
JjangSW <jjang6251@gmail.com>
2026-02-28 16:34:12 +0900

    (lesson 4) 컨트랙트 ether withdraw

--- smart contract/zombie/contracts/4/zombiehelper4.sol
+++ smart contract/zombie/contracts/4/zombiehelper4.sol
@@ -4,13 +4,27 @@ pragma solidity ^0.8.27;
 import "./zombiefeeding4.sol";
 
 contract ZombieHelper4 is ZombieFeeding4 {
-
     // levelUpFee를 받기 위한 0.001 ether 정의
     uint levelUpFee = 0.001 ether;
 
     modifier aboveLevel(uint _level, uint _zombieId) {
         require(zombies[_zombieId].level >= _level);
         _;
+    }
+
+    /**
+        사용자가 컨트랙트에 이더를 보내게 되면, 해당 컨트랙트의 이더리움 계좌에 이더가 저장되고 거기에 갇히게 된다.
+        -> 컨트랙트 주인이 이더를 인출하는 함수를 만들지 않는다면.
+
+        따라서 아래 withdraw 함수와 같이 컨트랙트 주인만이 계좌로 출금할 수 있도록 한다.
+     */
+    function withdraw() external onlyOwner {
+        (bool success, ) = payable(owner).call{value: address(this).balance}("");
+        require(success, "Transfer failed");
+    }
+
+    function setLevelUpFee(uint _fee) external onlyOwner {
+        levelUpFee = _fee;
     }
 
     function changeName(
