fc208037affba240d0ceae95c63dde8c5d64bc44
JjangSW <jjang6251@gmail.com>
2026-03-02 18:18:52 +0900

    (lesson 4) 난수함수 생성

--- smart contract/zombie/contracts/4/zombiebattle4.sol
+++ smart contract/zombie/contracts/4/zombiebattle4.sol
@@ -4,5 +4,18 @@ pragma solidity ^0.8.27;
 import "./zombiehelper4.sol";
 
 contract ZombieBattle4 is ZombieHelper4 {
-    
+    uint randNonce = 0;
+
+    /**
+        실제 이더리움에서 난수를 안전하게 만들기 위해서는 외부의 난수 함수에 접근할 수 있도록 해주는 오라클(oracle)을 사용한다.
+     */
+    function randMod(uint _modulus) internal returns (uint) {
+        randNonce++;
+        return
+            uint(
+                keccak256(
+                    abi.encodePacked(block.timestamp, msg.sender, randNonce)
+                )
+            ) % _modulus;
+    }
 }
